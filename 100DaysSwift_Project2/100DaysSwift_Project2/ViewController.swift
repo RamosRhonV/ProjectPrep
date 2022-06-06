b0321f9d62aac8c9aa560e5ec79cb77f3729a31b
RamosRhonV <61070300+RamosRhonV@users.noreply.github.com>
2022-06-06 16:01:32 -0400

    project2 started

---
+++ 100DaysSwift_Project2/100DaysSwift_Project2/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  100DaysSwift_Project2
+//
+//  Created by Consultant on 6/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
