463fccd404548130a0cbf950d61ac072432a068b
RamosRhonV <61070300+RamosRhonV@users.noreply.github.com>
2022-06-08 15:12:24 -0400

    project 9 complete

---
+++ 100DaysSwift_Project9/100DaysSwift_Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  100DaysSwift_Project7
+//
+//  Created by Consultant on 6/8/22.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ 100DaysSwift_Project9/100DaysSwift_Project7/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  100DaysSwift_Project7
+//
+//  Created by Consultant on 6/8/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var petitions = [Petition]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        performSelector(inBackground: #selector(fetchJSON), with: nil)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showCredits))
+    }
+    
+    @objc func fetchJSON() {
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            // let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            // let urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                // parsing data
+                self.parse(json: data)
+                return
+            }
+        }
+        
+        performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+    }
+    
+    @objc func showCredits() {
+        let ac = UIAlertController(title: "Credits", message: "All data shown comes from the \"We The People API\" of the Whitehouse.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func showError() {
+        
+        DispatchQueue.main.async {
+            let ac = UIAlertController(title: "Loading error", message: "Tjere was a problem loading the feed. Please check your internet connection then try again.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(ac, animated: true)
+        }
+    }
+    
+    @objc func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+        } else {
+            performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
