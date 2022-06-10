fe5d4a5895db34d8f96fafa41e71934c7165214c
RamosRhonV <61070300+RamosRhonV@users.noreply.github.com>
2022-06-10 09:41:00 -0400

    project 16 complete

---
+++ 100DaysSwift_Project16/100DaysSwift_Proejct16/Capital.swift
@@ -0,0 +1,22 @@
+//
+//  Capital.swift
+//  100DaysSwift_Proejct16
+//
+//  Created by Consultant on 6/10/22.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
