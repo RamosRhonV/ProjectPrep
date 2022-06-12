8afad022a826780b1aa876b3b498753a709c2c9a
RamosRhonV <61070300+RamosRhonV@users.noreply.github.com>
2022-06-12 15:59:43 -0400

    project 19 start

---
+++ 100DaysSwift_Project19/100DaysSwift_Project19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  100DaysSwift_Project19
+//
+//  Created by Consultant on 6/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 100DaysSwift_Project19/Presents User Interface/ActionViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ActionViewController.swift
+//  Presents User Interface
+//
+//  Created by Consultant on 6/12/22.
+//
+
+import UIKit
+import MobileCoreServices
+import UniformTypeIdentifiers
+
+class ActionViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
+            if let itemProvider = inputItem.attachments?.first {
+                itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) {
+                    [weak self] (dict, error) in
+                    // stuff
+                }
+            }
+        }
+    }
+}
